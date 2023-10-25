3a43f8f2ccb7ad25fd1470189823152fcc53b565
Leonardo Ávila <leonardo.avila98@outlook.com>
2023-10-25 15:37:58 -0300

    ..

--- main.tf
+++ main.tf
@@ -21,19 +21,8 @@ locals {
   archive_path = "./${local.binary_name}.zip"
 }
 
-variable "aws_access_key" {
-  type = string
-}
-
-variable "aws_secret_key" {
-  type = string
-}
-
 provider "aws" {
   region = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-
   default_tags {
     tags = {
       app = "lambda-jwt"
