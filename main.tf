6309e29062a04307445dae6d30d1c2d69caae3a3
Leonardo Ávila <leonardo.avila98@outlook.com>
2023-10-25 15:03:09 -0300

    using aws credentials secrets

--- main.tf
+++ main.tf
@@ -21,18 +21,18 @@ locals {
   archive_path = "./${local.binary_name}.zip"
 }
 
-# variable "aws_access_key" {
-#   type = string
-# }
+variable "aws_access_key" {
+  type = string
+}
 
-# variable "aws_secret_key" {
-#   type = string
-# }
+variable "aws_secret_key" {
+  type = string
+}
 
 provider "aws" {
   region = "us-east-1"
-  # access_key = var.aws_access_key
-  # secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 
   default_tags {
     tags = {
