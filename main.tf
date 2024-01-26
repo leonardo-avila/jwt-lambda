17a3ab81e7f96127ab131b79f9fc39d1c589879d
Leonardo Ávila <leonardo.avila98@outlook.com>
2024-01-26 19:41:14 -0300

    update terraform

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ terraform {
   required_version = ">= 1.3.7"
 
   backend "s3" {
-    bucket                  = "jwt-lambda-terraform"
-    key                     = "jwt-lambda-terraform"
-    region                  = "us-east-1"
+    bucket                  = "terraform-buckets-food-totem"
+    key                     = "jwt-lambda/terraform.tfstate"
+    region                  = "us-west-2"
   }
 }
 
@@ -28,33 +28,7 @@ locals {
 }
 
 provider "aws" {
-  region = "us-east-1"
-  default_tags {
-    tags = {
-      app = "lambda-jwt"
-    }
-  }
-}
-
-variable "jwt_secret_key" {
-  type = string
-}
-
-data "aws_iam_policy_document" "assume_lambda_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "lambda" {
-  name               = "AssumeLambdaRole"
-  description        = "Role for lambda to assume lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_lambda_role.json
+  region = "us-west-2"
 }
 
 # resource "null_resource" "function_binary" {
@@ -62,6 +36,14 @@ resource "aws_iam_role" "lambda" {
 #     command = "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ${local.binary_path} ${local.src_path}"
 #   }
 # }
+
+data "aws_security_group" "default" {
+  name = "default"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
 
 data "archive_file" "function_archive" {
   # depends_on = [null_resource.function_binary]
@@ -71,10 +53,10 @@ data "archive_file" "function_archive" {
   output_path = local.archive_path
 }
 
-resource "aws_lambda_function" "jwt-generator" {
-  function_name = "jwt-generator"
+resource "aws_lambda_function" "food-totem-jwt" {
+  function_name = "food-totem-jwt"
   description   = "JWT Generator for food totem"
-  role          = aws_iam_role.lambda.arn
+  role          = "arn:aws:iam::${var.lab_account_id}:role/LabRole"
   handler       = local.binary_name
   memory_size   = 128
 
@@ -88,70 +70,4 @@ resource "aws_lambda_function" "jwt-generator" {
       JWT_SECRET_KEY = var.jwt_secret_key
     }
   }
-}
-
-resource "aws_api_gateway_rest_api" "jwt-api" {
-  name        = "API Gateway for JWT Generator Lambda"
-  description = "Provides a gateway to call the JWT Generator Lambda"
-}
-
-resource "aws_api_gateway_resource" "jwt-proxy" {
-  rest_api_id = aws_api_gateway_rest_api.jwt-api.id
-  parent_id   = aws_api_gateway_rest_api.jwt-api.root_resource_id
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "jwt-proxy" {
-  rest_api_id   = aws_api_gateway_rest_api.jwt-api.id
-  resource_id   = aws_api_gateway_resource.jwt-proxy.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "jwt-lambda" {
-  rest_api_id = aws_api_gateway_rest_api.jwt-api.id
-  resource_id = aws_api_gateway_method.jwt-proxy.resource_id
-  http_method = aws_api_gateway_method.jwt-proxy.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.jwt-generator.invoke_arn
-}
-
-resource "aws_api_gateway_method" "jwt_proxy_root" {
-  rest_api_id   = aws_api_gateway_rest_api.jwt-api.id
-  resource_id   = aws_api_gateway_rest_api.jwt-api.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "jwt_lambda_root" {
-  rest_api_id = aws_api_gateway_rest_api.jwt-api.id
-  resource_id = aws_api_gateway_method.jwt_proxy_root.resource_id
-  http_method = aws_api_gateway_method.jwt_proxy_root.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.jwt-generator.invoke_arn
-}
-
-resource "aws_api_gateway_deployment" "jwt-api-deployment" {
-  depends_on = [
-    aws_api_gateway_integration.jwt-lambda,
-    aws_api_gateway_integration.jwt_lambda_root,
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.jwt-api.id
-  stage_name  = "prod"
-}
-
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.jwt-generator.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_rest_api.jwt-api.execution_arn}/*/*"
 }
